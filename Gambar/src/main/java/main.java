5429b037d690e0fe3eed8655ca684b5c21e88790
erwi saputra <86191067+erwinsaputraa@users.noreply.github.com>
2023-01-05 21:45:34 +0800

    Add files via upload

---
+++ Gambar/src/main/java/main.java
@@ -0,0 +1,9 @@
+import frame.FrameGambar;
+
+public class main {
+    public static void main(String[] args) {
+        FrameGambar viewframe = new FrameGambar();
+        viewframe.setVisible(true);
+    }
+}
+
